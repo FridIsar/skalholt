42f6aeeddad78bfa0cc51be4cc3167ebdd1c3835
Hallur Kristinn Hallsson <hallurkrist@gmail.com>
2021-10-18 17:33:20 +0000

    building images resized and put into temporary folder -BuildingsResized-

--- frontend/src/components/FindsIcon/FindsIcon.jsx
+++ frontend/src/components/FindsIcon/FindsIcon.jsx
@@ -14,7 +14,7 @@ export function FindsIcon({fGroup, fragments, index, iconData}) {
   const [anchorElHover, setAnchorElHover] = useState(null);
 
   //background image
-  var bImage = {backgroundImage: `url(/findsIcons/${fGroup}.jpg)`}
+  var bImage = {backgroundImage: `url(/findsIcons/${fGroup}.ico)`}
 
   function onclick(event) {
     setAnchorElClick(event?.target)
